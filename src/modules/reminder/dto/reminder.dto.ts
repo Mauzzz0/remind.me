af70061c40947ba88af458be24ddbfc4aaa9e8f8
Georgi Zarkya <zmeyfon@list.ru>
2025-03-21 18:07:55 +0300

    feat: Added validation for ReminderDto

--- src/modules/reminder/dto/reminder.dto.ts
+++ src/modules/reminder/dto/reminder.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class ReminderDto {
   @ApiProperty({ example: 1 })
+  @IsNumber()
   id: number;
 
   @ApiProperty({ example: 'To go for an interview tomorrow' })
+  @IsNotEmpty()
   description: string;
 
   @ApiProperty({ example: 'example@mail.ru' })
+  @IsEmail()
   email: string;
 
   @ApiProperty({ example: '2025-01-24T13:24:32.320Z' })
+  @IsDate()
   date: string;
 }
