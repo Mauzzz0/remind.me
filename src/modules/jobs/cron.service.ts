e782547f767a4c2692f772d6458f7ada9cdbdd57
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-03-28 20:34:58 +0300

    fix: Fixed Nest.JS dependency problem

--- src/modules/jobs/cron.service.ts
+++ src/modules/jobs/cron.service.ts
@@ -1,24 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
 import { Op } from 'sequelize';
 import { ReminderModel } from '../../database/postgres/models';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
-  constructor(private readonly reminderModel: typeof ReminderModel) {}
-
   @Cron(CronExpression.EVERY_MINUTE)
   async alert() {
     const date = new Date();
 
-    const reminder = await this.reminderModel.findOne({ where: { date: { [Op.lte]: date }, isSent: false } });
+    const reminder = await ReminderModel.findOne({ where: { date: { [Op.lte]: date }, isSent: false } });
 
     if (reminder) {
       this.logger.log(`Отправляем уведомление id=${reminder.id}`);
 
-      await this.reminderModel.update(
+      await ReminderModel.update(
         { isSent: true },
         {
           where: {
